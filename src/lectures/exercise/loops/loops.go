878621c41d6d58a62ea744e7ca124f1fa99732ab
Md Sakibul Hasan <sakibal623@gmail.com>
2023-12-07 21:55:30 +0600

    Loops.

--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -15,4 +15,33 @@ package main
 import "fmt"
 
 func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	// while loop
+	num := 10
+	for num > 0 {
+		fmt.Println(num)
+		num--
+	}
+
+	// Infinite loop
+	var x int = 2
+	for {
+		if x == 3 {
+			fmt.Println("X is now 3")
+			break
+		}
+		fmt.Println("X is not 3")
+		x++
+	}
+
+	// continue
+	for i := 0; i < 10; i++ {
+		if i == 3 {
+			continue
+		}
+		fmt.Println(i)
+	}
 }
