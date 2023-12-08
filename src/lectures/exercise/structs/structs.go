2e621aa67ca0694dc03b676e0d8821dfcba04a66
Md Sakibul Hasan <sakibal623@gmail.com>
2023-12-08 22:37:21 +0600

    Structures.

--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -19,6 +19,51 @@ package main
 
 import "fmt"
 
-func main() {
+type Passenger struct {
+	Name         string
+	TicketNumber int
+	Gov          bool
+}
 
+type Bus struct {
+	FrontSeat Passenger
+}
+
+type Coordinate struct {
+	height int
+	width  int
+}
+
+type Rectangle struct {
+	a Coordinate
+	b Coordinate
+}
+
+func area(rect Rectangle) int {
+	return (rect.a.height * rect.a.width) + (rect.b.height * rect.b.width)
+}
+
+func main() {
+	dora := Passenger{"Dora", 1, true}
+	fmt.Println(dora)
+	dora.Name = "HI"
+	fmt.Println(dora)
+
+	var (
+		nobi = Passenger{Name: "Nobi", TicketNumber: 2} // if i don't pass other value then have to specify names
+		sizu = Passenger{Name: "Sizu", TicketNumber: 4}
+	)
+	fmt.Println(nobi, sizu)
+
+	var soneo Passenger
+	soneo.Name = "Soneo"
+	soneo.TicketNumber = 5
+
+	// bus := Bus{FrontSeat: dora}
+	bus := Bus{dora}
+	fmt.Println(bus.FrontSeat.Name)
+
+	react := Rectangle{a: Coordinate{1, 2}, b: Coordinate{4, 5}}
+
+	fmt.Println("Area", area(react))
 }
