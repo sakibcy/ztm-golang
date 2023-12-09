e859630ff39961897e725f3628061abe167ed1f6
Md Sakibul Hasan <sakibal623@gmail.com>
2023-12-09 22:49:29 +0600

    Maps.

--- src/lectures/exercise/maps/maps.go
+++ src/lectures/exercise/maps/maps.go
@@ -20,7 +20,9 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	Online      = 0
@@ -29,6 +31,47 @@ const (
 	Retired     = 3
 )
 
+func printServerStatus(server map[string]int) {
+	fmt.Println("There are", len(server), " servers")
+}
+
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+	// myMap := map[string]int{
+	// 	"item 1": 18,
+	// 	"item 2": 2,
+	// 	"item 3": 3,
+	// }
+
+	myMap := make(map[string]int)
+
+	myMap["A"] = 40
+	myMap["B"] = 60
+	myMap["C"] = 90
+
+	fmt.Println(myMap["item 1"])
+	fmt.Println(myMap["A"])
+	fmt.Println(myMap["C"])
+
+	delete(myMap, "C")
+	fmt.Println(myMap["C"])
+
+	val, found := myMap["B"]
+	fmt.Println(val, found)
+	fmt.Println()
+
+	fmt.Println("Printing key value pairs")
+	for key, value := range myMap {
+		fmt.Println(key, value)
+	}
+
+	serverStatus := make(map[string]int)
+
+	for _, server := range servers {
+		serverStatus[server] = Online
+	}
+
+	fmt.Println()
+	fmt.Println("Printing servers")
+	printServerStatus(serverStatus)
 }
