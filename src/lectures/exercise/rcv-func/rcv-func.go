c2fc895ae639df1f5547dd965c5b37c0daa7264d
Md Sakibul Hasan <sakibal623@gmail.com>
2023-12-10 22:21:40 +0600

    Receiver Function.

--- src/lectures/exercise/rcv-func/rcv-func.go
+++ src/lectures/exercise/rcv-func/rcv-func.go
@@ -16,6 +16,47 @@ package main
 
 import "fmt"
 
-func main() {
+type Coordinate struct {
+	X, Y int
+}
 
+// Regular function
+func shiftBy(x, y int, coordinate *Coordinate) {
+	coordinate.X += x
+	coordinate.Y += y
+}
+
+// Pointer Receiver Function
+// it create a option to get this function with '.' like OOP
+func (coordinate *Coordinate) shiftBy(x, y int) {
+	coordinate.X += x
+	coordinate.Y += y
+}
+
+// Value Receiver Function
+// it create a option to get this function with '.' like OOP
+func (c Coordinate) shiftDist(other Coordinate) Coordinate {
+	return Coordinate{other.X - c.X, other.Y - c.Y}
+}
+
+func main() {
+	// =================== Pointer Receiver ===================== //
+	coordinate := Coordinate{5, 5}
+
+	// Regular function
+	shiftBy(1, 1, &coordinate)
+	fmt.Print("Regular -> X:", coordinate.X, " Y:", coordinate.Y)
+	fmt.Println()
+
+	// Pointer Receiver function
+	coordinate.shiftBy(2, 2)
+	fmt.Print("Pointer receiver -> X:", coordinate.X, " Y:", coordinate.Y)
+	fmt.Println()
+
+	// =========================== Value Reciever ===================== //
+	first := Coordinate{6, 6}
+	second := Coordinate{9, 9}
+
+	distance := first.shiftDist(second)
+	fmt.Println("Distance:", distance.X, distance.Y)
 }
